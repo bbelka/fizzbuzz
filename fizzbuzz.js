199f0393c6d87d7902c443c15181b7dcde6b8359
bbtwots <brettbelka@gmail.com>
2021-12-20 14:31:07 -0800

    finihs prompt

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,3 +2,15 @@
 //for each number, if it is divisible by 3, console log 'fizz'
     //if it is divisible by 5, console log 'buzz'
     //it its is divisible fy both 3 and 5, consolelog 'fizzbuzz'
+    //if not divisible by 3 or 5, console log the number
+
+
+
+
+
+//output:
+//1
+//2
+//fizz
+//4
+//buzz
