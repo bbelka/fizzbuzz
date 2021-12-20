122cf05d6d4402008d5a9e1194df3baf551f8ff8
bbtwots <brettbelka@gmail.com>
2021-12-20 14:24:55 -0800

    setup

---
+++ fizzbuzz.js
@@ -0,0 +1,4 @@
+//write a loop up to 100
+//for each number, if it is divisible by 3, console log 'fizz'
+    //if it is divisible by 5, console log 'buzz'
+    //it its is divisible fy both 3 and 5, consolelog 'fizzbuzz'
